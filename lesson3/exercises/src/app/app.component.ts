4b0cf7d673191df23ac148111b8c229ba3ad9ab0
Lisa A <lastembo@gmail.com>
2019-10-26 21:40:25 -0500

    lesson3 exercises complete

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,6 +12,36 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = true;
+
+  gutterCheck(width, height) {
+    if (width > 260000 || width < 0 || height < 0 || height > 340000) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    } else if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+    } else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    } else {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    }
+    this.gutterCheck(this.width, this.height);
+  }
 
   handleTakeOff() {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
@@ -20,34 +50,31 @@ export class AppComponent {
       this.height = 10000;
       this.width = 0;
       this.message = 'Shuttle in flight.';
+      this.takeOffEnabled = false;
     }
   }
 
-  handleLand() {
-    let result = window.confirm('Are you ready to Land the shuttle?');
-    if (result) {
-      this.color = 'orange';
-      this.height = 10000;
-      this.width = 0;
-      this.message = 'Shuttle is landing.';
-    }
+  handleLand(rocketImage) {
+    window.alert('The shuttle is landing.');
+    this.color = 'orange';
+    this.height = 0;
+    this.width = 0;
+    this.message = 'Shuttle landed.';
+    this.takeOffEnabled = true;
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
   }
 
-  handleAbortMission() {
-    let result = window.confirm('Are your sure you want to Abort Mission?');
+  handleMissionAbort(rocketImage) {
+    let result = window.confirm('Are you sure you want to end the mission?');
     if (result) {
       this.color = 'red';
       this.height = 0;
       this.width = 0;
-      this.message = 'Shuttle Mission is Aborted.';
+      this.message = 'Mission aborted.';
+      this.takeOffEnabled = true;
+      rocketImage.style.bottom = '0px';
+      rocketImage.style.left = '0px';
     }
   }
-
-  moveRocket(rocketImage, direction) {
-    if (direction === 'right') {
-    let movement = parseInt(rocketImage.style.left) + 10 + 'px';
-    rocketImage.style.left = movement;
-    this.width = this.width + 10000;
-    }
- }
 }
