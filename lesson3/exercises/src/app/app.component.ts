3938fd1e4d3c2a550892b384e9ee5697446203ba
Lisa A <lastembo@gmail.com>
2019-10-26 21:25:13 -0500

    lesson3 exercise moverocket rigth button complete

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,42 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff() {
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result) {
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+
+  handleLand() {
+    let result = window.confirm('Are you ready to Land the shuttle?');
+    if (result) {
+      this.color = 'orange';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle is landing.';
+    }
+  }
+
+  handleAbortMission() {
+    let result = window.confirm('Are your sure you want to Abort Mission?');
+    if (result) {
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Shuttle Mission is Aborted.';
+    }
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+    let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width = this.width + 10000;
+    }
+ }
 }
