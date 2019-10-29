430eff746e096b2c82d94379e69a5cacf7ac8a23
Lisa A <lastembo@gmail.com>
2019-10-29 00:04:00 -0500

    added search functionality

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -9,10 +9,12 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+    this.displayList = [];
 
     window.fetch(satellitesUrl).then(function (response) {
       response.json().then(function (data) {
@@ -24,11 +26,26 @@ export class AppComponent {
           let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
           this.sourceList.push(satellite);
+          // make a copy of the sourceList to be shown to the user
+          this.displayList = this.sourceList.slice(0);
         }
-
       }.bind(this));
     }.bind(this));
 
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for (let i = 0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
   }
 
 }
