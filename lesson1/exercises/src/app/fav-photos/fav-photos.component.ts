09d04c5a882212c9e72f9876bee815c11f04d469
Lisa A <lastembo@gmail.com>
2019-10-29 21:44:08 -0500

    lesson1 excercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Movie Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.dorisdaymagic.com/uploads/1/1/6/0/11608548/8445995.png?602';
+  image3 = 'http://www.gstatic.com/tv/thumb/v22vodart/34314/p34314_v_v8_ak.jpg';
 
   constructor() { }
 
